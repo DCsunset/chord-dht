1d68f56c41e864e434b4cedfa4648b191df59082
DCsunset <DCsunset@users.noreply.github.com>
2022-03-06 11:17:51 -0500

    WIP: fix node type and RPC type

--- src/chord/node.rs
+++ src/chord/node.rs
@@ -10,91 +10,119 @@ const NUM_BITS: usize = size_of::<Digest>() * 8;
 
 // Data part of the node
 #[derive(Debug, Clone)]
-struct NodeData {
+struct Node {
 	addr: String,
-	id: Digest,
-	successor: Digest,
-	predecessor: Digest
+	id: Digest
 }
 
 #[derive(Clone)]
 struct Finger {
 	start: Digest,
-	node: Digest
+	node: Option<Node>
 }
 
 #[tarpc::service]
-trait NodeRPC {
-	async fn get_data() -> NodeData;
+trait NodeService {
+	async fn get_node() -> Node;
+	async fn get_predecessor() -> Option<Node>;
+	async fn get_successor() -> Option<Node>;
 
-	async fn find_successor(id: Digest) -> Digest;
-	async fn find_predecessor(id: Digest) -> NodeData;
-	async fn closest_preceding_finger(id: Digest) -> Digest;
-	async fn notify(id: Digest);
+	async fn find_successor(id: Digest) -> Node;
+	async fn find_predecessor(id: Digest) -> Node;
+	async fn closest_preceding_finger(id: Digest) -> Node;
+	async fn notify(node: Node);
 }
 
-pub struct Node {
-	data: NodeData,
+pub struct NodeServer {
+	node: Node,
+	successor: Option<Node>,
+	predecessor: Option<Node>,
 	finger_table: [Finger; NUM_BITS],
 	// connection to remote nodes
-	connection_map: HashMap<Digest, Node>
+	connection_map: HashMap<Digest, NodeServiceClient>
 }
 
-impl Node {
-	pub fn new() -> Node {
-		Node {
-			data: NodeData {
-				id: 0,
-				successor: 0,
-				predecessor: 0,
-				addr: String::from("")
-			},
+impl NodeServer {
+	pub fn new(node: &Node) -> NodeServer {
+		NodeServer {
+			node: node.clone(),
+			successor: None,
+			predecessor: None,
 			finger_table: [Finger {
 				start: 0,
-				node: 0
+				node: None
 			}; 64],
 			connection_map: HashMap::new()
 		}
 	}
+	
+	pub async fn get_connection(&mut self, node: &Node) -> &NodeServiceClient {
+		match self.connection_map.get(&node.id) {
+			Some(c) => c,
+			// TODO: connect to the node
+			None => panic!()
+		}
+	}
 
-	pub async fn join(&mut self, id: Digest) {
-		self.data.predecessor = 0;
-		let node = self.connection_map.get(&id).unwrap();
-		self.data.successor = node.find_successor(context::current(), id).await;
+	pub async fn join(&mut self, node: &Node) {
+		self.predecessor = None;
+		let n = self.get_connection(node).await;
+		self.successor = Some(n.find_successor(context::current(), node.id).await.unwrap());
 	}
 
 	pub async fn stabilize(&mut self) {
 		let ctx = context::current();
-		let node = self.connection_map.get(&self.data.successor).unwrap();
-		let x = node.get_data(ctx).await.predecessor;
-		if x > self.data.id && x < self.data.successor {
-			self.data.successor = x;
+		let successor = match self.successor.as_ref() {
+			Some(s) => s.clone(),
+			None => return
+		};
+
+    let self_node = self.node.clone();
+		let n= self.get_connection(&successor).await;
+		let x= match n.get_predecessor(ctx).await.unwrap() {
+			Some(v) => v,
+			// Empty predecessor (TODO: log)
+			None => return
+		};
+		n.notify(ctx, self_node).await;
+
+		if x.id > self.node.id && x.id < successor.id {
+			self.successor = Some(x);
 		}
-		node.notify(ctx, self.data.id).await;
 	}
 
 	pub async fn fix_fingers(&mut self) {
 		let mut rng = rand::thread_rng();
 		let i = rng.gen_range(1..NUM_BITS);
-		self.finger_table[i].node = self.find_successor(context::current(), self.finger_table[i].start).await;
+		self.finger_table[i].node = Some(self.find_successor(context::current(), self.finger_table[i].start).await);
 	}
 }
 
-impl NodeRPC for Node {
-	type GetDataFut = future::Ready<NodeData>;
-	fn get_data(self, ctx: context::Context) -> Self::GetDataFut {
-		future::ready(self.data)
+impl NodeService for NodeServer {
+	type GetNodeFut = future::Ready<Node>;
+	fn get_node(self, ctx: context::Context) -> Self::GetNodeFut {
+		future::ready(self.node.clone())
 	}
 
-	type FindSuccessorFut = future::Ready<Digest>;
+	type GetPredecessorFut = future::Ready<Option<Node>>;
+	fn get_predecessor(self, ctx: context::Context) -> Self::GetPredecessorFut {
+		future::ready(self.predecessor.clone())
+	}
+
+	type GetSuccessorFut = future::Ready<Option<Node>>;
+	fn get_successor(self, ctx: context::Context) -> Self::GetPredecessorFut {
+		future::ready(self.successor.clone())
+	}
+
+	type FindSuccessorFut = future::Ready<Node>;
 	fn find_successor(self, ctx: context::Context, id: Digest) -> Self::FindSuccessorFut {
 		let n = self.find_predecessor(ctx, id).await;
 		future::ready(n.successor)
 	}
 
-	type FindPredecessorFut = future::Ready<NodeData>;
+	type FindPredecessorFut = future::Ready<Node>;
 	fn find_predecessor(self, ctx: context::Context, id: Digest) -> Self::FindPredecessorFut {
-		let n = self.data;
+		let n = self.node;
 		while id > n.id && id < n.successor {
 			let node = self.connection_map.get(&n.id);
 			let n_id = node.unwrap().closest_preceding_finger(ctx, id).await;
@@ -102,22 +130,30 @@ impl NodeRPC for Node {
 		future::ready(n)
 	}
 
-	type ClosestPrecedingFingerFut = future::Ready<Digest>;
+	type ClosestPrecedingFingerFut = future::Ready<Node>;
 	fn closest_preceding_finger(self, _: context::Context, id: Digest) -> Self::ClosestPrecedingFingerFut {
 		for i in (0..NUM_BITS).rev() {
-			let n = self.finger_table[i].node;
-			if n > id && n < self.data.id {
-				return future::ready(n);
-			}
+			match self.finger_table[i].node.as_ref() {
+				Some(n) => if n.id > id && n.id < self.node.id {
+					return future::ready(n.clone())
+				},
+				None => ()
+			};
 		}
-		future::ready(self.data.id)
+		future::ready(self.node.clone())
 	}
 
 	type NotifyFut = future::Ready<()>;
-	fn notify(self, _: context::Context, id: Digest) -> Self::NotifyFut {
-		if self.data.predecessor == 0 || (id > self.data.predecessor && id < self.data.id) {
-			self.data.predecessor = id;
-		}
+	fn notify(self, _: context::Context, node: Node) -> Self::NotifyFut {
+    let new_pred = match self.predecessor {
+			Some(v) => if node.id > v.id && node.id < self.node.id {
+				node
+			} else {
+				v
+			},
+			None => node
+		};
+    self.predecessor = Some(new_pred);
 		future::ready(())
 	}
 }
