c3edf58b55547bbaec1777b592dc5bcf1ad4b30d
DCsunset <DCsunset@users.noreply.github.com>
2022-03-18 22:05:40 -0400

    test: implement basic test

--- tests/figure_3b.rs
+++ tests/figure_3b.rs
@@ -1,5 +1,7 @@
 // Test based on Figure 3 (b) in Chord paper
 use chord_rust::{chord::Node, server::start_server, client::setup_client};
+use tarpc::context;
+use std::time::Duration;
 
 #[tokio::test]
 async fn test_figure_3b() {
@@ -23,13 +25,35 @@ async fn test_figure_3b() {
 
 	let s0 = start_server(n0.clone(), None);
 	tokio::spawn(s0);
+	tokio::time::sleep(Duration::from_millis(50)).await;
 	let c0 = setup_client(&n0.addr).await;
+	c0.stabilize_rpc(context::current()).await.unwrap();
+	// single-node ring
+	assert_eq!(c0.get_predecessor_rpc(context::current()).await.unwrap().unwrap().id, 0);
+	assert_eq!(c0.get_successor_rpc(context::current()).await.unwrap().unwrap().id, 0);
 
 	let s1 = start_server(n1.clone(), Some(n0.clone()));
 	tokio::spawn(s1);
+	tokio::time::sleep(Duration::from_millis(50)).await;
 	let c1 = setup_client(&n1.addr).await;
+	c0.stabilize_rpc(context::current()).await.unwrap();
+	c1.stabilize_rpc(context::current()).await.unwrap();
 
-	let s3 = start_server(n3.clone(), Some(n1.clone()));
-	tokio::spawn(s3);
-	let c3 = setup_client(&n3.addr).await;
+	assert_eq!(c1.get_predecessor_rpc(context::current()).await.unwrap().unwrap().id, 0);
+	assert_eq!(c1.get_successor_rpc(context::current()).await.unwrap().unwrap().id, 0);
+	assert_eq!(c0.get_predecessor_rpc(context::current()).await.unwrap().unwrap().id, 1);
+	assert_eq!(c0.get_successor_rpc(context::current()).await.unwrap().unwrap().id, 1);
+
+
+	// let s3 = start_server(n3.clone(), Some(n1.clone()));
+	// tokio::spawn(s3);
+	// tokio::time::sleep(Duration::from_millis(50)).await;
+	// let c3 = setup_client(&n3.addr).await;
+	// c0.stabilize_rpc(context::current()).await.unwrap();
+	// c1.stabilize_rpc(context::current()).await.unwrap();
+	// c3.stabilize_rpc(context::current()).await.unwrap();
+
+	// assert_eq!(c3.get_predecessor_rpc(context::current()).await.unwrap().unwrap().id, 1);
+	// assert_eq!(c1.get_predecessor_rpc(context::current()).await.unwrap().unwrap().id, 0);
+	// assert_eq!(c0.get_predecessor_rpc(context::current()).await.unwrap().unwrap().id, 3);
 }
