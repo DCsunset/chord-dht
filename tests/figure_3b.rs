2d6480d824902f16ce400ffcb49ccdda19db189d
DCsunset <DCsunset@users.noreply.github.com>
2022-03-21 15:25:29 -0400

    test: add test for figure 5a

--- tests/figure_3b.rs
+++ tests/figure_3b.rs
@@ -2,9 +2,9 @@
 use chord_rust::{chord::{Node, NodeServer, Config}, client::setup_client};
 use tarpc::context;
 
-/// Test figure 3b without fixing finger_table
+/// Test figure 3b, 5a
 #[tokio::test]
-async fn test_figure_3b() -> anyhow::Result<()> {
+async fn test_node_metadata() -> anyhow::Result<()> {
 	env_logger::init();
 
 	// Node 0
@@ -21,6 +21,11 @@ async fn test_figure_3b() -> anyhow::Result<()> {
 	let n3 = Node {
 		addr: "localhost:9803".to_string(),
 		id: 3
+	};
+	// Node 6
+	let n6 = Node {
+		addr: "localhost:9806".to_string(),
+		id: 6
 	};
 
 	let mut s0 = NodeServer::new(&n0);
@@ -68,6 +73,21 @@ async fn test_figure_3b() -> anyhow::Result<()> {
 	assert_eq!(c3.get_predecessor_rpc(context::current()).await.unwrap().unwrap().id, 1);
 	assert_eq!(c1.get_predecessor_rpc(context::current()).await.unwrap().unwrap().id, 0);
 	assert_eq!(c0.get_predecessor_rpc(context::current()).await.unwrap().unwrap().id, 3);
+
+
+	// Node 6 joins node 0
+	let mut s6 = NodeServer::new(&n6);
+	config.join_node = Some(n0.clone());
+	s6.start(&config).await?;
+	let c6 = setup_client(&n6.addr).await;
+	c6.stabilize_rpc(context::current()).await.unwrap();
+	c3.stabilize_rpc(context::current()).await.unwrap();
+	c1.stabilize_rpc(context::current()).await.unwrap();
+	c0.stabilize_rpc(context::current()).await.unwrap();
+	assert_eq!(c6.get_predecessor_rpc(context::current()).await.unwrap().unwrap().id, 0);
+	assert_eq!(c0.get_predecessor_rpc(context::current()).await.unwrap().unwrap().id, 1);
+	assert_eq!(c1.get_predecessor_rpc(context::current()).await.unwrap().unwrap().id, 3);
+	assert_eq!(c3.get_predecessor_rpc(context::current()).await.unwrap().unwrap().id, 6);
 
 	Ok(())
 }
