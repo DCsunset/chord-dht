b36b1d0fe0d103c64c862bc7ef180ad14ee36e52
DCsunset <DCsunset@users.noreply.github.com>
2022-03-23 21:40:12 -0400

    test: add kv_store integration test

--- tests/kv_store.rs
+++ tests/kv_store.rs
@@ -1,5 +1,27 @@
-use chord_rust::chord::{Node, NodeServer, calculate_hash};
-use rand::Rng;
+use chord_rust::{
+	chord::{
+		ring::{
+			NUM_BITS,
+			in_range
+		},
+		config::Config,
+		Node,
+		NodeServer,
+		calculate_hash
+	},
+	client::setup_client
+};
+use rand::{
+	prelude::*,
+	Rng
+};
+use tarpc::context;
+
+async fn fix_all_fingers(server: &mut NodeServer) {
+	for i in 1..NUM_BITS {
+		server.fix_finger(i).await;
+	}
+}
 
 // Generate key whose digest is in range (start, end]
 fn generate_key_in_range<T: Rng>(rng: &mut T, start: u64, end: u64) -> Vec<u8> {
@@ -7,7 +29,7 @@ fn generate_key_in_range<T: Rng>(rng: &mut T, start: u64, end: u64) -> Vec<u8> {
 	loop {
 		let key = rng.gen::<[u8; 8]>();
 		let digest = calculate_hash(&key);
-		if digest > start && digest <= end {
+		if in_range(digest, start, end) || digest == end {
 			return Vec::from(key);
 		}
 	}
@@ -39,7 +61,80 @@ async fn test_kv_store() -> anyhow::Result<()> {
 		id: u64::MAX / 4 * 3
 	};
 
+	let mut s0 = NodeServer::new(&n0);
+	// Disable auto fix_finger and stabilize
+	let mut config = Config {
+		join_node: None,
+		fix_finger_interval: 0,
+		stabilize_interval: 0
+	};
+	s0.start(&config).await?;
+	let c0 = setup_client(&n0.addr).await;
+	s0.stabilize().await;
 
+	let mut s1 = NodeServer::new(&n1);
+	config.join_node = Some(n0.clone());
+	s1.start(&config).await?;
+	let c1 = setup_client(&n1.addr).await;
+	s1.stabilize().await;
+	s0.stabilize().await;
+
+	fix_all_fingers(&mut s0).await;
+	fix_all_fingers(&mut s1).await;
+
+	let mut s3 = NodeServer::new(&n3);
+	config.join_node = Some(n1.clone());
+	s3.start(&config).await?;
+	let c3 = setup_client(&n3.addr).await;
+	s3.stabilize().await;
+	s1.stabilize().await;
+	s0.stabilize().await;
+
+	// See finger table in Figure 3b
+	fix_all_fingers(&mut s0).await;
+	fix_all_fingers(&mut s1).await;
+	fix_all_fingers(&mut s3).await;
+
+	// Node 6 joins node 0
+	let mut s6 = NodeServer::new(&n6);
+	config.join_node = Some(n0.clone());
+	s6.start(&config).await?;
+	let c6 = setup_client(&n6.addr).await;
+	s6.stabilize().await;
+	s3.stabilize().await;
+	s1.stabilize().await;
+	s0.stabilize().await;
+
+	// See finger table in Figure 6a
+	fix_all_fingers(&mut s0).await;
+	fix_all_fingers(&mut s1).await;
+	fix_all_fingers(&mut s3).await;
+	fix_all_fingers(&mut s6).await;
+
+
+	let mut rng = StdRng::seed_from_u64(0);
+	// k1 should be placed at n1
+	let k1 = generate_key_in_range(&mut rng, n0.id, n1.id);
+	let v1 = vec![1u8];
+	c0.set_rpc(context::current(), k1.clone(), Some(v1.clone())).await.unwrap();
+	assert_eq!(c0.get_rpc(context::current(), k1.clone()).await.unwrap().unwrap(), v1);
+	assert_eq!(c0.get_local_rpc(context::current(), k1.clone()).await.unwrap(), None);
+	assert_eq!(c1.get_rpc(context::current(), k1.clone()).await.unwrap().unwrap(), v1);
+	assert_eq!(c1.get_local_rpc(context::current(), k1.clone()).await.unwrap().unwrap(), v1);
+
+	// k2 should be placed at n3
+	let k2 = generate_key_in_range(&mut rng, n1.id, n3.id);
+	let v2 = vec![2u8];
+	c6.set_rpc(context::current(), k2.clone(), Some(v2.clone())).await.unwrap();
+	assert_eq!(c0.get_rpc(context::current(), k2.clone()).await.unwrap().unwrap(), v2);
+	assert_eq!(c0.get_local_rpc(context::current(), k2.clone()).await.unwrap(), None);
+	assert_eq!(c3.get_rpc(context::current(), k2.clone()).await.unwrap().unwrap(), v2);
+	assert_eq!(c3.get_local_rpc(context::current(), k2.clone()).await.unwrap().unwrap(), v2);
+
+	// delete k1
+	c3.set_rpc(context::current(), k1.clone(), None).await.unwrap();
+	assert_eq!(c0.get_rpc(context::current(), k1.clone()).await.unwrap(), None);
+	assert_eq!(c1.get_local_rpc(context::current(), k1.clone()).await.unwrap(), None);
 
 	Ok(())
 }
